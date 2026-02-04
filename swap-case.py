4c8e4468023349314b2b0c28551a33b84ae7b42f
ruthzu <ruthalemu75@gmail.com>
2026-02-04 15:29:12 +0300

    Add HackerRank solutions

--- swap-case.py
+++ swap-case.py
@@ -1,7 +1,13 @@
 def swap_case(s):
+    st=""
     for i in s:
-        if i
-    return
+        if i.islower():
+           st = st+i.upper()
+        elif i.isupper():
+            st=st+i.lower()
+        else:
+            st=st+i
+    return st
 
 if __name__ == '__main__':
     s = input()
