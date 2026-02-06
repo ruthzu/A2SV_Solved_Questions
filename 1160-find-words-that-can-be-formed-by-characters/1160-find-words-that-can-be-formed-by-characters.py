0a43400eed1a9b8a5eb79c84103fc796d7e165e3
Ruth Alemu <ruthalemu75@gmail.com>
2026-02-06 10:22:06 +0300

    Time: 7 ms (98.32%), Space: 19.9 MB (18%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -3,32 +3,19 @@ class Solution:
         
         
         ans=0
-        freq_chars=defaultdict(int)
-        for c in chars:
-            freq_chars[c]+=1
-   
-        def helper(word,freq_chars):
+        def helper(word,chars):
 
             count=0
             currcount=0
-            freq_word=defaultdict(int)
-
             for c in word:
-                freq_word[c]+=1
-            # print(freq_word,freq_chars)
-            
-            for c in word:
-                if freq_chars[c]<freq_word[c]:
-                    print(c,freq_chars[c],freq_word[c])
+                if word.count(c)>chars.count(c):
                     currcount=0
                     break
                 currcount+=1
             count+=currcount
             currcount=0
-            print(count)
             return count
         for word in words:
-            print(helper(word,freq_chars))
-            ans+=helper(word,freq_chars)
+            ans+=helper(word,chars)
         return ans
                 
