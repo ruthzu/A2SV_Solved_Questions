0ae6be2d062c2b617f5f8702d75775700124f347
Ruth Alemu <ruthalemu75@gmail.com>
2026-02-20 20:21:31 +0300

    Time: 4 ms (15.88%), Space: 19.4 MB (51.03%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -2,13 +2,8 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         
         citations.sort()
-        count = Counter(citations)
         for i in range(len(citations)):
             j = len(citations) - i
-            if citations[i] == j:
+            if citations[i] >= j:
                 return j
-        if len(citations) == 1 and citations[0] != 0:
-            return 1
-        if len(count) == 1:
-            return citations[0]
-        return 1
+        return 0
