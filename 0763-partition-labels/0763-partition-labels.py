33fc97a69bccfe9fa376e5f43da9ae25c2afacf9
Ruth Alemu <ruthalemu75@gmail.com>
2026-03-03 18:19:33 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,14 +1,24 @@
-from typing import List
-
-
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last = {ch: i for i, ch in enumerate(s)}
-        start, end = 0, 0
-        ans = []
-        for i, ch in enumerate(s):
-            end = max(end, last[ch])
-            if i == end:
-                ans.append(end - start + 1)
-                start = i + 1
+        count = defaultdict(list)
+        for i,ch in enumerate(s):
+            if count[ch] == []:
+                count[ch]= [i,i]
+            else:
+                count[ch][1]= i
+
+        val = sorted(list(count.values()))
+
+        ans=[]
+        s,e= val[0][0],val[0][-1]
+        for i in range(1,len(val)):
+            if val[i][0] < e:
+                e = max(e,val[i][-1])
+            else:
+                ans.append(e-s+1)
+                s=val[i][0]
+                e=val[i][-1]
+
+        ans.append(e-s+1)
         return ans
+
