0854ba9f6a381cbe99f2a7580e450df0b4a2f59e
Ruth Alemu <ruthalemu75@gmail.com>
2026-04-09 21:01:54 +0200

    Time: 0 ms (100%), Space: 24.5 MB (95.88%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -5,8 +5,8 @@ class Solution:
         ans = 0 
         while l <= r:
             mid = (l+r)//2
-            if r - mid + 1 >= citations[mid]:
-                ans = max(ans,mid + 1)
+            if len(citations) - mid <= citations[mid]:
+                ans = max(ans,len(citations) - mid)
                 r = mid - 1
             else:
                 l = mid + 1
